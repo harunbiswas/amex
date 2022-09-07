59fced63f7b389833b28120392b0bc2abedec6ba
Harun <harun@harun@gmail.com>
2022-09-08 01:42:45 +0600

    home page

--- main.js
+++ main.js
@@ -11,3 +11,27 @@ $(".fade").slick({
   autoplaySpeed: 5000,
   arrows: false,
 });
+
+// scroll
+const header = document.querySelector(".header");
+const brandImg = document.querySelector(".brand img");
+const scrollTop = document.querySelector(".scroll-top");
+
+window.addEventListener("scroll", () => {
+  if (window.scrollY > 150) {
+    header.classList.add("scroll");
+    brandImg.src =
+      "https://www.atmex.com.au/wp-content/uploads/2021/02/@mex-white-red-logo-220x84.png";
+    scrollTop.classList.add("active");
+  } else if (window.scrollY < 150) {
+    header.classList.remove("scroll");
+    brandImg.src =
+      "https://www.atmex.com.au/wp-content/uploads/2021/03/logo-with-whitge-bg-120x91.png";
+    scrollTop.classList.remove("active");
+  }
+});
+
+scrollTop.addEventListener("click", () => {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+});
